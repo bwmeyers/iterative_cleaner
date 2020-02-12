9314cab507f0357ed0d598a4122eb8f779cdadea
Bradley Meyers <bradley.meyers1993@gmail.com>
2020-02-12 14:11:14 -0800

    added setup.py so that this can be installed as a package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import sys
+
+reqs = ['numpy>=1.16.0', 'matplotlib>=3.0.0', 'scipy>=1.2.0']
+
+if sys.version_info < (3, 5):
+    sys.exit('iterative_cleaner requires Python 3.5+')
+
+setup(
+    name='iterative_cleaner',
+    author='Bradley Meyers',
+    author_email='bradley.meyers1993@gmail.com',
+    description='A package to clean RFI from folded pulsar data',
+    keywords="signal processing",
+    install_requires=reqs,
+    scripts=['iterative_cleaner.py']
+)
